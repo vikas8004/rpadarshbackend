8d4a808185cc01e3af21a1f47d3b9e7d0fb80679
vikas <vikas80046@gmail.com>
2024-02-01 12:09:41 +0530

    made some changes

--- src/controllers/getAllStudent.controller.js
+++ src/controllers/getAllStudent.controller.js
@@ -4,8 +4,11 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { errorHandler } from "../utils/erroHandler.js";
 const getAllStudents = asyncHandler(async (req, res) => {
   const foundStudents = await Admission.find();
+  console.log(foundStudents);
   if (!foundStudents) {
     throw new errorHandler("something went wrong");
   }
-  res.status(200).json(new ApiResponse(200, getAllStudents));
+  res.status(200).json(new ApiResponse(200,foundStudents));
 });
+
+export default getAllStudents;
--- src/routers/getAllStu.router.js
+++ src/routers/getAllStu.router.js
@@ -1,5 +1,6 @@
 import express from "express";
+import getAllStudents from "../controllers/getAllStudent.controller.js";
 const studentRouter = express.Router();
 
-studentRouter.route("/allstudents").get(getAllStu);
+studentRouter.route("/allstudents").get(getAllStudents);
 export default studentRouter;
